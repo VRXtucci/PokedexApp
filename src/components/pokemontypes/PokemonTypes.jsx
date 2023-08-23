b87458f76ff720943adf4d7206d02679315a814a
Jose Tucci <tucci.jose023@gmail.com>
2023-08-23 12:01:53 -0400

    Componente para cargar los colores de los tipos del Pokemon

---
+++ src/components/pokemontypes/PokemonTypes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PokemonTypes = {
+    steel:"#1e1e1e",
+    flying:"#006ab4",
+    water:"#017bac",
+    ice:"#00a3aa",
+    grass:"#00ac36",
+    bug:"#ecec00",
+    electric:"#a68c4e",
+    normal:"#e1a810",
+    rock:"#e05b00",
+    ground:"#f24000",
+    fire:"#ff6600",
+    fighting:"#ff0d0c",
+    fairy:"#ff5ea1",
+    psychic:"#ff1348",
+    poison:"#ff31db",
+    dragon:"#d25eff",
+    ghost:"#620f99",
+    sinister:"#871c48"
+    };
+
+export default PokemonTypes;
